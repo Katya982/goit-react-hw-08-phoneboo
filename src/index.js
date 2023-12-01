1ec25ed0965b6a2b93bc7eabd897bb754624cc3f
Katya982 <xamko98@gmail.com>
2023-12-01 20:23:01 +0200

    add hw 8

--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <Provider store={store}>
+    <BrowserRouter basename="/goit-react-hw-08-phonebook">
+      <PersistGate loading={null} persistor={persistor}>
+        <App />   
+      </PersistGate>
+    </BrowserRouter>
+  </Provider>
+);   
+
+
